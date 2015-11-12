5fefcd19a98cd97863cce28955c27358a74e8a99
SNatali <bossovivh@mail.ru>
2015-11-12 17:00:45 +0100

    commit 9

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 var link = document.querySelector(".title-form");
 var form = document.querySelector(".form"); 
+ 
 
  link.addEventListener("click", function(event) {
 	event.preventDefault();
 	form.classList.toggle("form-show");
  });
+
  // link.addEventListener("click", function(event) {
  // event.preventDefault(); 
  // form.classList.toggle("form-show"); 
